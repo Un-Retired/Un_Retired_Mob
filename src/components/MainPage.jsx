52d96be8f8f35e2a285e1042e5b97743b641e4c1
ejjang2030 <ejjang2030@gmail.com>
2025-02-03 00:44:12 +0900

    style : NavigationBar Alignment 수정

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -165,45 +165,45 @@ const MainPage = () => {
 
       <footer className='fixed bottom-0 left-0 right-0 h-[60px] bg-bc-black'>
         <div className='flex justify-between items-center h-full px-6'>
-          <div className='flex flex-col items-center'>
+          <div className='w-[45px] h-[45px] flex flex-col items-center'>
             <img
               src='assets/강의실.svg'
               alt='강의실'
               className='w-6 h-6'
             />
-            <span className='text-xs mt-1 text-bc-white'>강의실</span>
+            <span className='text-[10px] mt-1 text-bc-white'>강의실</span>
           </div>
-          <div className='flex flex-col items-center'>
+          <div className='w-[45px] h-[45px] flex flex-col items-center'>
             <img
               src='assets/캘린더.svg'
               alt='캘린더'
               className='w-6 h-6'
             />
-            <span className='text-xs mt-1 text-bc-white'>캘린더</span>
+            <span className='text-[10px] mt-1 text-bc-white'>캘린더</span>
           </div>
-          <div className='flex flex-col items-center'>
+          <div className='w-[45px] h-[45px] flex flex-col items-center'>
             <img
               src='assets/홈.svg'
               alt='홈'
               className='w-6 h-6'
             />
-            <span className='text-xs mt-1 text-bc-white'>홈</span>
+            <span className='text-[10px] mt-1 text-bc-white'>홈</span>
           </div>
-          <div className='flex flex-col items-center'>
+          <div className='w-[45px] h-[45px] flex flex-col items-center'>
             <img
               src='assets/커뮤니티.svg'
               alt='커뮤니티'
               className='w-6 h-6'
             />
-            <span className='text-xs mt-1 text-bc-white'>커뮤니티</span>
+            <span className='text-[10px] mt-1 text-bc-white'>커뮤니티</span>
           </div>
-          <div className='flex flex-col items-center'>
+          <div className='w-[45px] h-[45px] flex flex-col items-center'>
             <img
               src='assets/마이페이지.svg'
               alt='마이페이지'
               className='w-6 h-6'
             />
-            <span className='text-xs mt-1 text-bc-white'>마이페이지</span>
+            <span className='text-[10px] mt-1 text-bc-white'>마이페이지</span>
           </div>
         </div>
       </footer>
